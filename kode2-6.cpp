bb0c7983676efeaf8987cd2d41e805acdccf962b
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-08-19 12:47:52 +0700

    Add files via upload

---
+++ kode2-6.cpp
@@ -0,0 +1,29 @@
+/**********************************************************
+*  PEMROGRAMAN C++                                        *
+*                                                         *
+*  Author  : Gesa Rizky Nugraha                           *
+*  Email   : gesarizkynugraha@gmail.com                   *
+*                                                         *
+*  Website : karenabelajar.blogspotcom                    *
+*                                                         *
+***********************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int X;
+
+  // Menampilkan nilai X sebelum 
+  // dilakukan pengisian nilai (assignment)
+  cout<<"Nilai X sebelum assignment: "<<X<<endl;
+
+  // mengisi nilai 10 ke dalam variabel X
+  X = 10;
+  // Menampilkan nilai X setelah 
+  // dilakukan pengisian nilai (assignment)
+  cout<<"Nilai X sesudah assignment: "<<X<<endl;
+
+  return 0;
+}
